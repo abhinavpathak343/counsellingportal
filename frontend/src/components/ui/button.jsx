d8a44777fc1e4b0643c06cccab6c17728fe5f5c6
abhinav <abhinavpathak343@gmail.com>
2025-01-29 18:26:31 +0530

    change in BUTTONp.jsx

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -1,10 +1,10 @@
 export function Button({ children, className, ...props }) {
   return (
-    <button
+    <Button
       className={`py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white rounded-lg ${className}`}
       {...props}
     >
       {children}
-    </button>
+    </Button>
   );
 }
